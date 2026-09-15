712019e1eae43760b85cab115c2c6a56533b98b2
langs <1009523335@qq.com>
2026-09-15 14:45:26 +0800

    step

--- swap/main.cc
+++ swap/main.cc
@@ -1,11 +1,9 @@
 void swap();
 
-int buf[2];
-// int buf[2] = {1, 2};
+int buf[2] = {1, 2};
 
 int main()
 {
-    buf = {1, 2};
     swap();
     return 0;
 }
---
+++ swap/main.s
@@ -0,0 +1,31 @@
+	.file	"main.cc"
+	.text
+	.globl	buf
+	.data
+	.align 8
+	.type	buf, @object
+	.size	buf, 8
+buf:
+	.long	1
+	.long	2
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	call	_Z4swapv
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 8.5.0 20210514 (Red Hat 8.5.0-4)"
+	.section	.note.GNU-stack,"",@progbits
